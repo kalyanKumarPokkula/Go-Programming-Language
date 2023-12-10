b479846e9ced8c0773c6e6a38b266f17b3a49884
kalyan <kalyanKumarPokkula@gmail.com>
2023-12-10 19:49:12 +0530

    learned basic variable dataTypes constants

---
+++ week-1/go.mod
@@ -0,0 +1,3 @@
+module week-1
+
+go 1.21.3
---
+++ week-1/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main () {
+
+	// Variable 
+
+	var i, j, k int = 1,2,3
+
+	const fruit = "apple"
+	
+	fmt.Println(i,j,k)
+
+	// Data types 
+
+	// bool
+
+	var b bool = true
+
+	// string
+
+	var str string = "My name is kalyan"
+
+	// int  int8  int16  int32  int64
+	var num int = 23;
+	// uint uint8 uint16 uint32 uint64 uintptr
+
+	// byte // alias for uint8
+
+	// rune // alias for int32
+	// 	// represents a Unicode code point
+
+	// float32 float64
+
+	var flo float32 = 2.00
+
+	// complex64 complex128
+
+	
+
+	
+
+	fmt.Println(str, num, flo , b)
+
+}
+
